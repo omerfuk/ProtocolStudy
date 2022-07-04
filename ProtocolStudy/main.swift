12945506a1dc83753825f2c1c4b49a881e29fb68
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-04 16:26:34 +0300

    .

--- ProtocolStudy/main.swift
+++ ProtocolStudy/main.swift
@@ -7,5 +7,64 @@
 
 import Foundation
 
-print("Hello, World!")
+
+protocol Running {
+    
+    func myRun()
+    
+}
+
+class Animal {
+    func test() {
+        print("test")
+    }
+    
+}
+
+class Dog: Running {
+    
+    func myRun() {
+        print("Running")
+    }
+    
+    
+    
+}
+
+let barley = Dog()
+barley.myRun()
+
+class Cat:Animal, Running {
+    func myRun() {
+        print("Cat running")
+    }
+    
+
+}
+let cat = Cat()
+cat.myRun()
+cat.test()
+
+
+
+//cat.running()
+
+class Turtle:Animal {
+    
+    
+}
+
+let leonardo = Turtle()
+//leonardo.running()
+
+struct Bird: Running {
+    func myRun() {
+        print("Bird running")
+    }
+    
+   
+}
+
+let tweety = Bird()
+tweety.myRun()
 
